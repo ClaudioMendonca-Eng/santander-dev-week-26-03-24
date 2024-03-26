9a5fe0234cada6e2f8077065611658ebc54b5340
Claudio Mendonça <contato@claudiomendonca.eng.br>
2024-03-26 10:51:18 -0300

    Renomear o arquivo SantanderDevWeek26032024Application para Application.java e suas atualizações

--- src/main/java/me/dio/sdw2024/Application.java
+++ src/main/java/me/dio/sdw2024/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SantanderDevWeek260324Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SantanderDevWeek260324Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }

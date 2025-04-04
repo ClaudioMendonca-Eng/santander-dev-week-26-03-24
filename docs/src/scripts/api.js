4c294c68652f521e7bd2550ce6d17342868f6e66
Claudio Mendonça <68253329+ClaudioMendonca-Eng@users.noreply.github.com>
2025-04-04 17:01:18 -0300

    Update api.js

--- docs/src/scripts/api.js
+++ docs/src/scripts/api.js
@@ -5,8 +5,8 @@ async function exibirTextoNaTela(tag, texto) {
 }
 
 const routes = {
-  champions: "http://sdw24.sa-east-1.elasticbeanstalk.com/champions",
-  ask: "http://sdw24.sa-east-1.elasticbeanstalk.com/champions/{id}/ask",
+  champions: "https://corsproxy.io/?url=http://sdw24.sa-east-1.elasticbeanstalk.com/champions",
+  ask: "https://corsproxy.io/?url=http://sdw24.sa-east-1.elasticbeanstalk.com/champions/{id}/ask",
 };
 //const routes = {
 //  champions: "https://cors-proxy.fringe.zone/http://sdw24.sa-east-1.elasticbeanstalk.com/champions",
